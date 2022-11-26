b9864ea3efd741223d26130ea77d894a4457e3ef
bralexx <bruek.aiu@phystech.edu>
2022-11-26 21:05:28 +0300

    user info style final

--- src/components/AccountInfo.js
+++ src/components/AccountInfo.js
@@ -1,7 +1,7 @@
 function AccountInfo() {
   return (
     <div id='user-info-container'>
-      <img id='user-info-image' src='../resources/user_image.jpg' alt='user'/>
+      <img id='user-info-image' src='/images/user_image.jpg' alt='user'/>
       <p id='user-info-username'>username</p>
       <button id='user-info-logout-button'>Log out</button>
     </div>
