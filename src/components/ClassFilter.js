ed00c415fbdc4df60c642ff158918b1c3f5e0e7f
bralexx <bruek.aiu@phystech.edu>
2022-11-27 15:41:33 +0300

    radio input fix

--- src/components/ClassFilter.js
+++ src/components/ClassFilter.js
@@ -1,7 +1,7 @@
 function classes_parser(filter_name, filter_type, class_name) {
   return (
     <div key={filter_name + class_name + '_filter_option'} className='filter-element-option-container'>
-      <input key={filter_type + class_name} type={filter_type === 'class_mul'? 'checkbox' : 'radio'} id={class_name} name={class_name} value={1}></input>
+      <input key={filter_type + class_name} type={filter_type === 'class_mul'? 'checkbox' : 'radio'} id={class_name} name={filter_name} value={1}></input>
       <label className='filter-element-option-title'>{class_name}</label>
     </div>
   )
@@ -10,10 +10,10 @@ function classes_parser(filter_name, filter_type, class_name) {
 function ClassFilter(filter_info) {
   let wrapper = class_name => classes_parser(filter_info.name, filter_info.type, class_name)
   return (
-    <div className='filter-element-container'>
+    <filedset id={filter_info.name + '_filed'} className='filter-element-container'>
       <p className='filter-element-title'>{filter_info.name}</p>
         {filter_info.classes.map(wrapper)}
-    </div>
+    </filedset>
   );
 }
 
